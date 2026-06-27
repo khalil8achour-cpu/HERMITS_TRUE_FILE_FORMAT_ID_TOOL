ddf4830c2b2df36781d2bc4368f3a691c7af99d3
khalil8achour-cpu <khalil8achour@gmail.com>
2026-06-27 13:55:39 +0100

    final version

--- tfformat/install.sh
+++ tfformat/install.sh
@@ -2,15 +2,9 @@
 echo "========================================"
 echo "Installing tfformat..."
 echo "========================================"
-
-# Install the Python script
 sudo cp tfformat/main.py /usr/local/bin/tfformat
 sudo chmod +x /usr/local/bin/tfformat
-
-# Install the database
 sudo cp tfformat/magicDB.json /usr/local/bin/
-
-# Make it executable
 sudo chmod +x /usr/local/bin/tfformat
 
 echo "✅ Done! Type 'tfformat' anywhere."
